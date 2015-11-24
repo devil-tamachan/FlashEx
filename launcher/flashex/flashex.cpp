69e6228e42278ebf846c99afb814967cd24a2b79
devil.tamachan <devil.tamachan@gmail.com>
2015-11-24 20:33:27 +0900

    crcチェック追加

--- launcher/flashex/flashex.cpp
+++ launcher/flashex/flashex.cpp
@@ -6,6 +6,7 @@
 #include <windows.h>
 #include <stdio.h>
 #include "createprocess.h"
+#include "crc.h"
 
 #pragma warning(disable : 4996)
 
@@ -18,6 +19,9 @@ int _tmain(int argc, _TCHAR* argv[])
   // get the full path of ropguarddll.dll
   char dllpath[1000];
   char *filename;
+  long s;
+  crc crcExe;
+
   if (!GetModuleFileName(NULL, dllpath, 980))
   {
     printf("Error: could not obtain current executable path\n");
@@ -30,6 +34,32 @@ int _tmain(int argc, _TCHAR* argv[])
   }
   filename++;
   strcpy(filename, "cpicker.dll");
+  
+  FILE *fp = fopen("Flash4.exe", "rb");
+  if(fp==NULL)return 0;
+  fseek(fp, 0L, SEEK_END);
+  s = ftell(fp);
+  fseek(fp, 0L, SEEK_SET);
+  unsigned char *buf = (unsigned char*)malloc(s);
+  if(buf==NULL)
+  {
+    fclose(fp);
+    return 0;
+  }
+  fread(buf, 1, s, fp);
+  fclose(fp);
+
+	crcInit();
+  crcExe = crcFast(buf, s);
+  free(buf);
+
+  if(crcExe!=0x0B4A/*Flash4J*/ && crcExe!=0xAA51/*Flash4J 2GB+*/)
+  {
+    char dbg[50];
+    sprintf(dbg, "Unsupport Flash4.exe (CRC: 0x%04X)", crcExe);
+    MessageBox(NULL, dbg, "Error", MB_OK);
+    return 0;
+  }
 
   char *pCmdline = (char *)malloc(5000);
   pCmdline[0] = NULL;
